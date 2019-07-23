19e2ba738f0bed485fc722849f500fff10f87423
Kate Rakelly <krakelly@berkeley.edu>
2019-07-23 10:54:31 -0700

    change reset angles to work for laudri

--- src/sawyer_control/configs/ros_config.py
+++ src/sawyer_control/configs/ros_config.py
@@ -18,9 +18,10 @@ JOINT_NAMES = ['right_j0',
                'right_j6'
                ]
 LINK_NAMES = ['right_l2', 'right_l3', 'right_l4', 'right_l5', 'right_l6', '_hand']
-RESET_ANGLES = np.array(
-    [0.15262207,  0.91435349, -2.02594233,  1.6647979, -2.41721773, 1.14999604, -2.47703505]
-)
+#RESET_ANGLES = np.array(
+    #[0.15262207,  0.91435349, -2.02594233,  1.6647979, -2.41721773, 1.14999604, -2.47703505]
+#)
+RESET_ANGLES = np.array([.132, -.757, -.654, 1.434, .474, 1.110, -.221])
 
 RESET_DICT = dict(zip(JOINT_NAMES, RESET_ANGLES))
 #POSITION_CONTROL_EE_ORIENTATION=Quaternion(
