-fix peg reward to consider both ee position and pose. -correctly calculate quat angle differences + set scaling for reward terms. -fix reset to move ee upward before reset. -fix bug in ik_server which was going to preset position instead of given one. -new webcame server to do concat on cv side (instead of having 2 separate servers).

src/sawyer_control/envs/mslac_peg.py
@@ -6,10 +6,16 @@ import gym
 import os
 
 from pyquaternion import Quaternion
+
 from sawyer_control.coordinates import quat_2_euler, euler_2_rot, euler_2_quat
 from sawyer_control.envs.sawyer_env_base import SawyerEnvBase
 from sawyer_control.core.serializable import Serializable
+from sawyer_control.envs.rotation_utils import quat_magnitude, quat_difference
 
+import cv2
+import copy
+import rospy
+from sawyer_control.srv import image
 
 class MslacPegInsertionEnv(SawyerEnvBase):
 
@@ -29,19 +35,34 @@ class MslacPegInsertionEnv(SawyerEnvBase):
         SawyerEnvBase.__init__(self, **kwargs)
 
         # params
-        self.timestep = 0.1 # this env will be running at 1/x Hz
-        self.im_width = 32
-        self.im_height = 32
+        self.timestep = 0.25 # this env will be running at 1/x Hz
         self.num_joint_dof = 7
 
         # note: this env is currently written only for this mode
-        assert self.action_mode=='joint_position'
+        assert self.action_mode in ['joint_position', 'joint_delta_position']
 
-        # limits
-        joint_vel_limit = 0.07 #deg/sec
-        self.limits_joint_vel = np.array([joint_vel_limit]*self.num_joint_dof)
-        self.limits_lows_joint_pos = np.array([0, -0.9, -0.8, 1.6, 0.6, -0.7, -1.5])
-        self.limits_highs_joint_pos = np.array([0.3, -0.7, -0.1, 2.1, 2.7, 0.7, 1.5])
+        # vel limits
+        joint_vel_lim = 0.07 #deg/sec
+        self.limits_lows_joint_vel = -np.array([joint_vel_lim]*self.num_joint_dof)
+        self.limits_highs_joint_vel = np.array([joint_vel_lim]*self.num_joint_dof)
+
+        # position limits (tight)
+        # self.limits_lows_joint_pos = np.array([0, -0.9, -0.8, 1.6, 0.6, -0.7, -1.5])
+        # self.limits_highs_joint_pos = np.array([0.3, -0.7, -0.1, 2.1, 2.7, 0.7, 1.5])
+
+        # position limits (tight) with peg/camera attached
+        # self.limits_lows_joint_pos = np.array([0, -0.9, -0.8, 1.6, 0.6, -0.7, -1.5])
+        # self.limits_highs_joint_pos = np.array([0.3, -0.8, -0.3, 2.1, 2.7, 0.7, 1.5])
+
+        # position limits (loose, need safety box!)
+        self.limits_lows_joint_pos = np.array([0, -1, -1, 1.3, 0.6, -0.7, -1.5])
+        self.limits_highs_joint_pos = np.array([0.4, -0.6, 0, 2.5, 2.7, 0.7, 1.5])
+
+        # safety box (calculated for our current single-task peg setup)
+        self.safety_box_ee_low = np.array([0.36,-0.26,0.196])
+        self.safety_box_ee_high = np.array([0.8,0.26,0.5])
+
+        # ee limits
         self.limits_lows_ee_pos = -1*np.ones(3)
         self.limits_highs_ee_pos = np.ones(3)
         self.limits_lows_ee_angles = -1*np.ones(4)
@@ -53,23 +74,40 @@ class MslacPegInsertionEnv(SawyerEnvBase):
 
         # ranges
         self.action_range = self.action_highs-self.action_lows
-        self.joint_range = (self.limits_highs_joint_pos - self.limits_lows_joint_pos)  
+        self.joint_pos_range = (self.limits_highs_joint_pos - self.limits_lows_joint_pos)
+        self.joint_vel_range = (self.limits_highs_joint_vel - self.limits_lows_joint_vel)
 
         # reset position (note: this is the "0" of our new (-1,1) action range)
-        self.reset_joint_positions = self.limits_lows_joint_pos + self.joint_range/2.0
-        self.reset_duration = 6.0 # seconds to allow for reset
+        #self.reset_joint_positions = self.limits_lows_joint_pos + self.joint_pos_range/2.0 # modified by Tony
+        self.reset_joint_positions = np.array([ 0.38670802, -0.76212013, -0.91199511,  1.92950678,  1.03574216, 0.83555567,  1.1440537 ])
+        self.reset_duration = 3.0 # seconds to allow for reset
 
         # goal pose is when the peg is correctly inserted
+        # x: 0.459949938269
+        # y: 0.0171827931642
+        # z: 0.195978444238
+
         # TODO just made this goal up
-        self.goal_ee_position = np.array([0.45, 0.1, 0.23])
-        self.goal_ee_orientation = np.array([1, 0, 0, 0])
-        self.goal_ee = np.concatenate((self.goal_ee_position, self.goal_ee_orientation))
+        # self.goal_ee_position = np.array([ 0.51412338, -0.04801155,  0.20031133])
+        # # self.goal_ee = self.goal_ee_position
+        # self.goal_ee_orientation = np.array([1, 0, 0, 0])
+        self.goal_ee = np.array([ 0.52665746, -0.03103374,  0.19041668,  0.10857889,  0.99396318, -0.01117118, -0.01108997])
 
         # reset robot to initialize
         self.reset()
 
+        # Added by Tony
+
+        self.sparse_reward = False  # TODO Tony: hardcoded. Also not expecting to run sparse reward on robot
+
     ####################################
     ####################################
+
+    # added by Tony
+    def override_action_mode(self, mode):
+        assert mode in ['joint_position', 'joint_delta_position']
+        self.action_mode = mode
+        print("ACTION MODE: ", self.action_mode)
 
     def _set_observation_space(self):
         ''' [14] : observation is [7] joint angles + [3] ee pos + [4] ee angles '''
@@ -86,9 +124,43 @@ class MslacPegInsertionEnv(SawyerEnvBase):
     ####################################
     ####################################
 
-    def _get_image(self):
-        # TODO haven't integrated this function with the mslac codebase yet
-        return self.get_image(self.im_width, self.im_height)
+    def _get_image(self, width, height, double_camera):
+        if double_camera:
+            image =  self.get_double_image(output_width=width, output_height=height)
+        else:
+            image = self.get_image(width=width, height=height)
+        return image
+
+    def get_double_image(self, output_width=84, output_height=84): # override base_env, for double camera
+        double_image = self.request_double_image()
+
+
+        input_w = 480*2 # input width
+        input_h = 640 # input height
+
+        if (double_image is None):
+            raise Exception('Unable to get double image(s) from image server')
+        double_image = np.array(double_image).reshape((input_w, input_h, 3))
+        double_image = double_image[:, :int(input_w/2), :]  # crop such that width:high = 2:1: make height: 640->480
+
+        processed_w = input_w
+        processed_h = input_w/2
+
+        double_image = copy.deepcopy(double_image)
+        double_image = cv2.resize(double_image, (0, 0), fx=output_width/processed_w, fy=output_height/processed_h, interpolation=cv2.INTER_AREA)
+        double_image = np.asarray(double_image).reshape((output_width, output_height, 3))[:, :, ::-1]
+        return double_image # np.expand_dims(combined_image, axis=0)
+
+    def request_double_image(self):
+        rospy.wait_for_service('images_webcam_double')
+        try:
+            request = rospy.ServiceProxy('images_webcam_double', image, persistent=True)
+            obs = request()
+            return (
+                    obs.image
+            )
+        except rospy.ServiceException as e:
+            print(e)
 
     def _get_obs(self):
         ''' [7] joint angles + [7] ee pose '''
@@ -99,20 +171,71 @@ class MslacPegInsertionEnv(SawyerEnvBase):
     ####################################
     ####################################
 
+    def quat_score(self, q1, q2):
+        assert len(q1) == len(q2) == 4
+        return quat_magnitude(quat_difference(q1, q2))
+
+
     def compute_rewards(self, obs, action=None):
-        # TODO
-        return 0 ##-np.linalg.norm(obs[:3] - self.goal_pose[:3])
+        # want indices 7-13
+        ee_pose = obs[self.num_joint_dof:self.num_joint_dof+7] # same as env._get_endeffector_pose(): gives 3 + 4
+        goal_pose = self.goal_ee
+
+        # distance between the points
+        score_dist = np.linalg.norm(ee_pose[:3] - goal_pose[:3])
+        # score_angle = quat_magnitude(quat_difference(ee_pose[3:], goal_pose[3:]))
+        score_angle = self.quat_score(ee_pose[3:], goal_pose[3:])
+
+        alpha = 1.0
+        beta = 0.1
+        score = alpha*score_dist + beta*score_angle
+
+        #print("in hardware getting reward/score ", score, "TODO: make sure number is reasonable")
+
+        dist = 5*score
+
+        assert self.sparse_reward is False # for now
+
+        # # Sparse reward setting
+        # if self.sparse_reward:
+        #     dist = min(dist, self.truncation_dist) # if dist too large: return the reward at truncate_dist
+
+        # use GPS cost function: log + quadratic encourages precision near insertion
+        reward = -(dist ** 2 + math.log10(dist ** 2 + 1e-5))
+
+        # if self.sparse_reward:
+        #     # offset the whole reward such that when dist>truncation_dist, the reward will be exactly 0
+        #     reward = reward - (-(self.truncation_dist ** 2 + math.log10(self.truncation_dist ** 2 + 1e-5)))
+
+        print("in peg env: reward is:", reward)
+
+        return reward
 
     def step(self, action):
 
-        # convert from given (-1,1) to joint positions (low,high)
-        action = np.clip(action, -1, 1)
-        action_scaled = (((action - self.action_lows) * self.joint_range) / self.action_range) + self.limits_lows_joint_pos
+        if self.action_mode=='joint_position':
+
+            # clip incoming action
+            desired_joint_positions = np.clip(action, -1, 1)
+
+            # convert from given (-1,1) to joint pos limits (low,high)
+            desired_joint_positions_scaled = (((desired_joint_positions - self.action_lows) * self.joint_pos_range) / self.action_range) + self.limits_lows_joint_pos
+
+        elif self.action_mode=='joint_delta_position':
+
+            # clip the incoming vel
+            delta_joint_pos = np.clip(action, -1, 1)
+
+            # convert from given (-1,1) to joint vel limits (low,high) # tony: notice vel_range is actually delta range
+            delta_joint_pos_scaled = (((delta_joint_pos - self.action_lows) * self.joint_vel_range) / self.action_range) + self.limits_lows_joint_vel
+
+            # turn the delta into an action position
+            curr_pos = self._get_joint_angles()
+            desired_joint_positions_scaled = curr_pos + delta_joint_pos_scaled
+
 
         # enforce joint velocity limits on this scaled action
-        feasible_scaled_action = self.make_feasible(action_scaled)
-
-        # TODO safety box?
+        feasible_scaled_action = self.make_feasible(desired_joint_positions_scaled)
 
         # take a step
         self._act(feasible_scaled_action, self.timestep)
@@ -123,7 +246,7 @@ class MslacPegInsertionEnv(SawyerEnvBase):
         # reward/etc. after taking the step
         reward = self.compute_rewards(obs)
         done = False
-        info = self._get_info()
+        info = None #self._get_info()
         return obs, reward, done, info
 
     def make_feasible(self, desired_positions):
@@ -131,8 +254,8 @@ class MslacPegInsertionEnv(SawyerEnvBase):
         curr_positions = self._get_joint_angles()
 
         # compare the implied vel to the max vel allowed
-        max_vel = self.limits_joint_vel*self.timestep
-        implied_vel = np.abs(desired_positions-curr_positions)
+        max_vel = self.limits_highs_joint_vel*self.timestep #[7]
+        implied_vel = np.abs(desired_positions-curr_positions) #[7]
 
         # limit the vel 
         actual_vel = np.min([implied_vel, max_vel], axis=0)
@@ -141,11 +264,39 @@ class MslacPegInsertionEnv(SawyerEnvBase):
         sign = np.sign(desired_positions-curr_positions)
         actual_difference = sign*actual_vel
         feasible_positions = curr_positions+actual_difference
+
+        # find the actual position, taking safety box into account
+        feasible_positions = self.consider_safety_box(curr_positions, feasible_positions)
+
         return feasible_positions
 
+    def consider_safety_box(self, curr_joint_positions, desired_joint_positions):
+        # find desired_ee_position
+        transformation_matrix = self._get_ee_fk(desired_joint_positions)
+        desired_ee_position = np.array([transformation_matrix[3], transformation_matrix[7], transformation_matrix[11]])
+
+        # if that's outside safety box, don't move the robot
+        if np.any(desired_ee_position>self.safety_box_ee_high) or np.any(desired_ee_position<self.safety_box_ee_low):
+            return curr_joint_positions
+        else:
+            return desired_joint_positions
+
     def reset(self):
-        # reset the arm to these positions
+
+        # move upward to make sure not stuck
+        self._move_ee_upward()
+
+        # move to reset position
         self._act(self.reset_joint_positions, self.reset_duration, reset=True)
 
         # return the observation
         return self._get_obs()
+
+    def _move_ee_upward(self):
+        curr_ee_pose = self._get_endeffector_pose()
+        target_position = curr_ee_pose[:3] + np.array([0, 0, 0.05])
+        target_quat = curr_ee_pose[3:]
+        target_ee_pose = np.concatenate([target_position, target_quat])
+
+        angles = self.request_ik_angles(target_ee_pose, self._get_joint_angles())
+        self._act(angles, self.reset_duration, reset=True)

src/sawyer_control/envs/mslac_reacher.py
@@ -116,51 +116,35 @@ class MslacReacherEnv(SawyerEnvBase):
 
     def _get_image(self, width, height, double_camera):
         if double_camera:
-            image =  self.get_double_image(width=width, height=height)
+            image =  self.get_double_image(output_width=width, output_height=height)
         else:
             image = self.get_image(width=width, height=height)
         return image
 
-    def get_double_image(self, width=84, height=84): # override base_env, for double camera
-        ee_image = self.request_ee_image()
-        overall_image = self.request_overall_image()
+    def get_double_image(self, output_width=84, output_height=84): # override base_env, for double camera
+        double_image = self.request_double_image()
 
-        input_w = 480 # input width
+
+        input_w = 480*2 # input width
         input_h = 640 # input height
 
-        if (overall_image is None) or (ee_image is None):
-            raise Exception('Unable to get one/both image(s) from image server')
-        ee_image = np.array(ee_image).reshape((input_w, input_h, 3))
-        ee_image = ee_image[:, :input_w]  # crop, so resize behaves correctly
-        overall_image = np.array(overall_image).reshape((input_w, input_h, 3))
-        overall_image = overall_image[:, :input_w]  # crop, so resize behaves correctly: now w * w
+        if (double_image is None):
+            raise Exception('Unable to get double image(s) from image server')
+        double_image = np.array(double_image).reshape((input_w, input_h, 3))
+        double_image = double_image[:, :int(input_w/2), :]  # crop such that width:high = 2:1: make height: 640->480
 
-        combined_image = self.combine_image(ee_image, overall_image)
+        processed_w = input_w
+        processed_h = input_w/2
 
-        combined_image = copy.deepcopy(combined_image)
-        combined_image = cv2.resize(combined_image, (0, 0), fx=width/(2*input_w), fy=height/input_w, interpolation=cv2.INTER_AREA)
-        combined_image = np.asarray(combined_image).reshape((width, height, 3))[:, :, ::-1]
-        return combined_image # np.expand_dims(combined_image, axis=0)
+        double_image = copy.deepcopy(double_image)
+        double_image = cv2.resize(double_image, (0, 0), fx=output_width/processed_w, fy=output_height/processed_h, interpolation=cv2.INTER_AREA)
+        double_image = np.asarray(double_image).reshape((output_width, output_height, 3))[:, :, ::-1]
+        return double_image # np.expand_dims(combined_image, axis=0)
 
-    def combine_image(self, image1, image2):
-        """naive version"""
-        return np.concatenate((image1, image2), axis=0) # width * 2
-
-    def request_ee_image(self):
-        rospy.wait_for_service('images_webcam_ee')
+    def request_double_image(self):
+        rospy.wait_for_service('images_webcam_double')
         try:
-            request = rospy.ServiceProxy('images_webcam_ee', image, persistent=True)
-            obs = request()
-            return (
-                    obs.image
-            )
-        except rospy.ServiceException as e:
-            print(e)
-
-    def request_overall_image(self):
-        rospy.wait_for_service('images_webcam_overall')
-        try:
-            request = rospy.ServiceProxy('images_webcam_overall', image, persistent=True)
+            request = rospy.ServiceProxy('images_webcam_double', image, persistent=True)
             obs = request()
             return (
                     obs.image
@@ -274,8 +258,21 @@ class MslacReacherEnv(SawyerEnvBase):
             return desired_joint_positions
 
     def reset(self):
-        # reset the arm to these positions
+
+        # move upward to make sure not stuck
+        self._move_ee_upward()
+
+        # move to reset position
         self._act(self.reset_joint_positions, self.reset_duration, reset=True)
 
         # return the observation
         return self._get_obs()
+
+    def _move_ee_upward(self):
+        curr_ee_pose = self._get_endeffector_pose()
+        target_position = curr_ee_pose[:3] + np.array([0, 0, 0.05])
+        target_quat = curr_ee_pose[3:]
+        target_ee_pose = np.concatenate([target_position, target_quat])
+
+        angles = self.request_ik_angles(target_ee_pose, self._get_joint_angles())
+        self._act(angles, self.reset_duration, reset=True)